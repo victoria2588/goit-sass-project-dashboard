6c9c723530142e31fa69d3f8c0a7a1eec81ab850
Victoriia D <eva_vica@i.ua>
2024-02-16 10:53:28 +0200

    commit 01

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require("postcss-sort-media-queries")({
-      sort: "mobile-first", // default value
+    require('postcss-sort-media-queries')({
+      sort: 'mobile-first', // default value
     }),
-    require("autoprefixer"),
+    // require("autoprefixer"),
   ],
 };
